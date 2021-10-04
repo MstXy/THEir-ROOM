ed6fa83d31a10c23c96cf25d77905e03158a5ea5
Chengyu Zhang <cz1627@nyu.edu>
2021-10-04 20:17:57 +0400

    update page

--- script.js
+++ script.js
@@ -10,5 +10,23 @@ video_end = function() {
   document.getElementById("video-container").style.animation = "fadeOut 2s";
   setTimeout(function() {
     document.getElementById("video-container").remove();
+    document.getElementById("intro-container").style.display = "block";
+    document.getElementById("intro-container").style.animation = "fadeIn 2s";
 }, 2000);
+
+}
+
+
+// navigation
+load_index = function() {
+  window.location.href = "index.html";
+}
+load_room = function() {
+  window.location.href = "explore.html";
+}
+load_item = function() {
+  window.location.href = "http://www.w3schools.com";
+}
+load_goal = function() {
+  window.location.href = "http://www.w3schools.com";
 }
